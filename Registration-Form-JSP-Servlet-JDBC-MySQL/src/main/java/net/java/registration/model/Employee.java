5a9c700624a98b23fa181b450802980fbf9f6556
Raju kumar <85837020+Hunter7388@users.noreply.github.com>
2022-03-07 00:13:50 +0530

    Add files via upload

---
+++ Registration-Form-JSP-Servlet-JDBC-MySQL/src/main/java/net/java/registration/model/Employee.java
@@ -0,0 +1,63 @@
+package net.java.registration.model;
+
+public class Employee {
+
+	private String FirstName;
+	private String secondName;
+	private String username;
+	private String password;
+	private String contact;
+	private String id;
+
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+		
+	}
+
+
+
+}
